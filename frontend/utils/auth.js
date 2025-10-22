607cc89498df4adad7c367421947aaff5653c96c
anwarnagori <ar1498137@gmail.com>
2025-10-23 01:09:16 +0500

    updated

--- frontend/utils/auth.js
+++ frontend/utils/auth.js
@@ -1,6 +1,5 @@
-// frontend/utils/auth.js
-export const isAdminAuthenticated = () => {
+export function isAdminAuthenticated() {
   if (typeof window === "undefined") return false;
   const token = localStorage.getItem("adminToken");
   return !!token;
-};
+}
